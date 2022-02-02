5c082fe8344fc4cee70ba45febf6fc27774275a7
Erwin Lara <75814707+elara711@users.noreply.github.com>
2022-02-02 03:29:07 +0000

    played with text

---
+++ pre01/Hello.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Hello {
+public static void main(String[] args){
+       System.out.println("Hello this world!");
+       System.out.println("");
+       System.out.println("Now is a good time for all good men to come to the aid of their country");
+    }
+}
