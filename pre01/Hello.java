f3e10cbd8959761142c30e221cc843f49460e844
Erwin Lara <75814707+elara711@users.noreply.github.com>
2022-04-02 20:56:11 +0000

    I updated my text

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -6,5 +6,7 @@ public static void main(String[] args){
        System.out.println("Hello this world!");
        System.out.println("");
        System.out.println("Now is a good time for all good men to come to the aid of their country");
+      System.out.println("I am redoing this program to update my Hello World program");
+
     }
 }
