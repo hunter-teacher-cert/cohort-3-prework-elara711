c336daf0b35dbbdd69e7ed9eefb4edd8d0dd23e1
Erwin Lara <75814707+elara711@users.noreply.github.com>
2022-04-03 23:03:04 +0000

    cleaned up code format

--- pre02/Time.java
+++ pre02/Time.java
@@ -3,21 +3,6 @@ import java.util.*;
 public class Time {
 public static void main(String[] args){
       
-
-    String day,month;
-    int date, year;
-
-    day = "Sunday";
-    month = "April";
-    date = 4;
-    year = 2022;
-
-      System.out.println("American format:");
-      System.out.println(day+" "+month+""+date+","+year);
-      System.out.println("European format:");
-      System.out.println(day+" "+date+" "+month+""+year);
-
-
 double hour, minute;
 double second;
 long start = System.currentTimeMillis( );
