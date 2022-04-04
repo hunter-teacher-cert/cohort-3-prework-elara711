7cd70686c07e280bcae2d6a51a516b57658d5a63
Erwin Lara <75814707+elara711@users.noreply.github.com>
2022-04-04 20:50:16 +0000

    initial code for Celsius

---
+++ pre02/Celsius.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Celsius {
+public static void main(String[] args){
+
+
+ System.out.println("Start");
+  
+// Exercise 2   Write a program that converts a temperature from Celsius to Fahrenheit.
+  
+  
+//(1) prompt the user for input
+    final double CON_MULTIPLE=9/5;
+    final int CON_CONSTANT=32; // 
+    int cels; // celsius variable 
+    double fahr; // fahrenheit variable
+    Scanner in = new Scanner(System.in);
+  
+
+        //System.out.print("Type something: ");
+        //line = in.nextLine();
+        //System.out.println("You said: " + line);
+
+        //System.out.print("Type something else: ");
+        //ine = in.nextLine();
+        //System.out.println("You also said: " + line);
+ 
+  
+//(2) read a double value from the keyboard, 
+  
+// (3) calculate the result, and 
+  
+//(4) format the output to one decimal place. For example, it should display "24.0 C = 75.2 F"
+
+// Formula for Celsius to Fahrenheit Conversion
+// F=C * (9/5) + 32
+// fahrenheit=CON_CONSTANT+(celsius * CON_MULTIPLE)
+
+
+ System.out.println("Complete");
+  
+    }
+}
